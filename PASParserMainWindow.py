cd7e196d68126164fb525fdd95c3a53838291f59
mbenthaier <mohammed.benthaier@sefi.net>
2017-06-13 16:03:59 +0800

    First slot

--- PASParserMainWindow.py
+++ PASParserMainWindow.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+
+
 from PyQt4 import QtGui, QtCore
 import sys
 
 import ui_MainWindow
 
-class Hello(QtGui.QMainWindow, ui_MainWindow.Ui_MainWindow):
+class PASParserMainWindow(QtGui.QMainWindow, ui_MainWindow.Ui_MainWindow):
 	def __init__(self, parent=None):
-		super(Hello, self).__init__(parent)
+		super(PASParserMainWindow, self).__init__(parent)
 		self.setupUi(self)
+
+	def on_action_Import_triggered(self):
+		print("Import")
 
 	def main(self):
 		self.show()
 
 if __name__=='__main__':
 	app = QtGui.QApplication(sys.argv)
-	imageViewer = Hello()
-	imageViewer.main()
+	pasParserMainWindow = PASParserMainWindow()
+	pasParserMainWindow.main()
 	app.exec_()
