93097a2eeba3729c24735103321a00397fa2f10c
mbenthaier <mohammed.benthaier@sefi.net>
2017-06-15 15:07:40 +0800

    open many dds files feature

--- PASParserMainWindow.py
+++ PASParserMainWindow.py
@@ -17,14 +17,9 @@ class PASParserMainWindow(QtGui.QMainWindow, ui_MainWindow.Ui_MainWindow):
     def __init__(self, parent=None):
         super(PASParserMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.pasDir = {}
-        self.model = PASParserTreeModel(self)
-        self.treeView.setModel(self.model)
-        self.treeView.setColumnWidth(0, 190)
-        self.treeView.setColumnWidth(1, 130)
-        self.treeView.setColumnWidth(2, 50)
-        self.treeView.setColumnWidth(3, 100)
-        self.treeView.clicked.connect(self.on_itemActivated)
+        self.pasDir = []
+        self.model = {}
+        self.treeView = {}
 
         self.iniParser = PASINIParser()
         self.objReader = PASObjReader()
@@ -33,32 +28,53 @@ class PASParserMainWindow(QtGui.QMainWindow, ui_MainWindow.Ui_MainWindow):
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         my_dir = os.sep.join([cur_dir, 'ECS-ELITE_1_C-1'])
 
-        self.pasDir[self.tabWidget.currentIndex()] = my_dir
-        self.tabWidget.setTabText(0, re.split(r'[/\\]', my_dir)[-1] )
-        objects = filter(lambda x: re.match(r'^[0-9A-Fa-f]+$', x), os.listdir(my_dir))
-        for obj in objects:
-            PASObjectNode(obj, '', '', '', '', self.model.root)
-        self.model.insertRows(0, len(objects), QtCore.QModelIndex())
+        self.loadDDS(my_dir)
 
 
     @QtCore.pyqtSlot() # signal with no arguments
     def on_action_Import_triggered(self):
         my_dir = QtGui.QFileDialog.getExistingDirectory(self, tr("Open a folder"), ".", QtGui.QFileDialog.ShowDirsOnly)
 
-        self.pasDir[self.tabWidget.currentIndex()] = my_dir
-        self.tabWidget.setTabText(0, re.split(r'[/\\]', my_dir)[-1] )
+        self.loadDDS(str(my_dir))
+
+    def loadDDS(self, fullPath):
+        if fullPath in self.pasDir:
+            print("{0}: folder already open".format(fullPath))
+            return
+
+        self.pasDir.append(fullPath)
+        shortPath = re.split(r'[/\\]', fullPath)[-1]
+
+        model = PASParserTreeModel(self)
+        self.model[fullPath] = model
+        treeView = QtGui.QTreeView()
+        self.treeView[fullPath] = treeView
+
+        treeView.setModel(self.model[fullPath])
+        treeView.setColumnWidth(0, 190)
+        treeView.setColumnWidth(1, 130)
+        treeView.setColumnWidth(2, 50)
+        treeView.setColumnWidth(3, 100)
+        treeView.clicked.connect(self.on_itemActivated)
+
+        tabIndex = self.tabWidget.addTab(treeView, shortPath)
+        # we save the full path in the toolTip we are gonna need it later (we can also use tabData)
+        self.tabWidget.setTabToolTip(tabIndex, fullPath)
 
         #find files whose name is a hexadecimal number
-        objects = filter(lambda x: re.match(r'^[0-9A-Fa-f]+$', x), os.listdir(my_dir))
+        objects = filter(lambda x: re.match(r'^[0-9A-Fa-f]+$', x), os.listdir(fullPath))
         for obj in objects:
-            PASObjectNode(obj, '', '', '', self.model.root)
-        self.model.insertRows(0, len(objects), QtCore.QModelIndex())
+            PASObjectNode(obj, '', '', '', None, model.root)
+        model.insertRows(0, len(objects), QtCore.QModelIndex())
+
 
     @QtCore.pyqtSlot(QtCore.QModelIndex) # signal with arguments
     def on_itemActivated(self, index):
-        if self.model.rowCount(index) == 0 and self.model.isChildOfRoot(index):
-            node = self.model.nodeFromIndex(index)
-            self.iniParser.parse(self.pasDir[self.tabWidget.currentIndex()], node.name)
+        path = str(self.tabWidget.tabToolTip(self.tabWidget.currentIndex()))
+        model = self.model[path]
+        if model.rowCount(index) == 0 and model.isChildOfRoot(index):
+            node = model.nodeFromIndex(index)
+            self.iniParser.parse(path, node.name)
             data = self.iniParser.getData()
             self.objReader[node.name].readData(data)
 
@@ -72,11 +88,13 @@ class PASParserMainWindow(QtGui.QMainWindow, ui_MainWindow.Ui_MainWindow):
                     for i in range(0, field.arraySize):
                         PASObjectNode(field.nameOfField + "[{}]".format(i), "byte {0} to {1}".format(field.range_[i][0], field.range_[i][1]),
                                             field.size, field.arraySize, self.objReader[node.name][field.nameOfField], arrayNode)
-                    self.model.insertRows(0, field.arraySize, self.model.index(i, 0, self.model.index(j, 0, index) ))
-            self.model.insertRows(0, self.objReader[node.name].nbFields(), index)
+                    model.insertRows(0, field.arraySize, model.index(i, 0, model.index(j, 0, index) ))
+            model.insertRows(0, self.objReader[node.name].nbFields(), index)
 
 
 
+    def closeEvent(self, event):
+        print("Bye bye")
 
     def main(self):
         self.show()
